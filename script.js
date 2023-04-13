ab594def43bbd4f1e2ddaca203af22e1f0d45bce
Ceejayludick <ceejayludick13@gmail.com>
2023-04-13 21:44:34 +0200

    update
    changing date and time setting

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-// On app load, get all tasks from localStorage
+// code to get data from localStorage
 window.onload = loadTasks;
 
 
-
-// On form submit add task
+// code to add a new task
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
   addTask();
 });
 
 function loadTasks() {
-  // check if localStorage has any tasks
-  // if not then return
   if (localStorage.getItem("tasks") == null) return;
-
-  // Get the tasks from localStorage and convert it to an array
+  // code to convert storage data to an array
   let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
 
-  // Loop through the tasks and add them to the list
+  // code to Loop tasks and add to list
   tasks.forEach(task => {
     const list = document.querySelector("ul");
     const li = document.createElement("li");
@@ -31,27 +27,25 @@ function loadTasks() {
 function addTask() {
   const task = document.querySelector("form input");
   const list = document.querySelector("ul");
-  // return if task is empty
   if (task.value === "") {
     alert("Please add some task!");
     return false;
   }
-  // check is task already exist
   if (document.querySelector(`input[value="${task.value}"]`)) {
     alert("Task already exist!");
     return false;
   }
 
-  // add task to local storage
+  // code to add task to local storage
   localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false }]));
 
-  // create list item, add innerHTML and append to ul
+  // code to create li, innerHTML and append ul
   const li = document.createElement("li");
   li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
       <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
       <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
   list.insertBefore(li, list.children[0]);
-  // clear input
+  // code for clearing input
   task.value = "";
 }
 
@@ -70,7 +64,7 @@ function removeTask(event) {
   let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
   tasks.forEach(task => {
     if (task.task === event.parentNode.children[1].value) {
-      // delete task
+      // code to delete task
       tasks.splice(tasks.indexOf(task), 1);
     }
   });
